903b4485ec0237711c213fb81db6aec7084760d5
bdb <bdb@ACA81E6F.ipt.aol.com>
2019-02-16 18:01:47 +0800

    Moya使用 集成类库

---
+++ RxSwift学习/RxSwift学习/RXSwift学习/48.结合Moya使用1：数据请求/MoyaUseViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MoyaUseViewController.swift
+//  RXSwift学习
+//
+//  Created by bdb on 2/16/19.
+//  Copyright © 2019 fulihao. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class MoyaUseViewController: UIViewController {
+
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        /*
+         Moya是一个基于Alamofire的更高网络请求封装抽象层。它可以对我们项目中的素有请求进行集中管理，方便开发与维护。同时Moya自身也提供了对RxSwift的扩展，通过与RxSwift的结合，能让Moya变得更加强大。下面我就通过样例演示如何使用“RxSwift + Moya”这个组合进行开发
+         */
+        
+        // 获取数据
+        DouBanProvider.rx.request(.channels)
+            .subscribe { (event) in
+                switch event {
+                case let .success(response):
+                    // 数据处理
+                        let str = String(data: response.data, encoding: String.Encoding.utf8)
+                    print("返回的数据是：",str ?? "")
+                case let .error(error):
+                    print("数据请求失败！错误原因：",error)
+                }
+        }.disposed(by: disposeBag)
+        
+    }
+}
--- RxSwift学习/RxSwift学习/RXSwift学习/AppDelegate.swift
+++ RxSwift学习/RxSwift学习/RXSwift学习/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UINavigationBar.appearance().tintColor = .white
+        let rootViewController: UIViewController = RxAlamofireBasicViewController()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
         return true
     }
 
